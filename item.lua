8c4dd48ce487874e8fda4dacb914355349ac727f
IcyLava <icylavah@gmail.com>
2018-10-31 19:26:54 +0200

    Implemented various media item functions

--- item.lua
+++ item.lua
@@ -21,6 +21,120 @@ function item:setSelected(selected)
 	return self
 end
 
+function item:getNotes()
+	return select(2, reaper.GetSetMediaItemInfo_String(self, 'P_NOTES', '', false))
+end
+
+function item:setNotes(notes)
+	reaper.GetSetMediaItemInfo_String(self, 'P_NOTES', notes, true)
+	
+	return self
+end
+
+function item:isMuted()
+	return reaper.GetMediaItemInfo_Value(self, 'B_MUTE') ~= 0
+end
+
+-- must call srlapi.refresh() afterwards
+function item:setMuted(muted)
+	reaper.SetMediaItemInfo_Value(self, 'B_MUTE', muted and 1 or 0)
+	return self
+end
+
+function item:isLooping()
+	return reaper.GetMediaItemInfo_Value(self, 'B_LOOPSRC') ~= 0
+end
+
+-- must call srlapi.refresh() afterwards
+function item:setLooping(looping)
+	reaper.SetMediaItemInfo_Value(self, 'B_LOOPSRC', looping and 1 or 0)
+	return self
+end
+
+function item:isLocked()
+	return reaper.GetMediaItemInfo_Value(self, 'C_LOCK') ~= 0
+end
+
+-- must call srlapi.refresh() afterwards
+function item:setLocked(locked)
+	reaper.SetMediaItemInfo_Value(self, 'C_LOCK', locked and 1 or 0)
+	return self
+end
+
+function item:getVolume()
+	return reaper.GetMediaItemInfo_Value(self, 'D_VOL')
+end
+
+-- must call srlapi.refresh() afterwards
+function item:setVolume(volume)
+	reaper.SetMediaItemInfo_Value(self, 'D_VOL', volume)
+	return self
+end
+
+function item:getPosition()
+	return reaper.GetMediaItemInfo_Value(self, 'D_POSITION')
+end
+
+function item:setPosition(position)
+	reaper.SetMediaItemInfo_Value(self, 'D_POSITION', position)
+	return self
+end
+
+function item:getLength()
+	return reaper.GetMediaItemInfo_Value(self, 'D_LENGTH')
+end
+
+function item:setLength(length)
+	reaper.SetMediaItemInfo_Value(self, 'D_LENGTH', length)
+	return self
+end
+
+function item:getSnapOffset()
+	return reaper.GetMediaItemInfo_Value(self, 'D_SNAPOFFSET')
+end
+
+-- must call srlapi.refresh() afterwards
+function item:setSnapOffset(offset)
+	reaper.SetMediaItemInfo_Value(self, 'D_SNAPOFFSET', offset)
+	return self
+end
+
+function item:getGroupID()
+	local id = reaper.GetMediaItemInfo_Value(self, 'I_GROUPID')
+	if id > 0 then return id end
+	return nil
+end
+
+-- must call srlapi.refresh() afterwards
+function item:setGroupID(id)
+	reaper.SetMediaItemInfo_Value(self, 'I_GROUPID', id or 0)
+	return self
+end
+
+function item:getColor()
+	if reaper.GetMediaItemInfo_Value(self, 'I_CUSTOMCOLOR') & 0x01000000 == 0 then
+		return nil
+	end
+	local r, g, b = reaper.ColorFromNative(reaper.GetMediaItemInfo_Value(self, 'I_CUSTOMCOLOR'))
+	
+	return r / 255, g / 255, b / 255
+end
+
+-- must call srlapi.refresh() afterwards
+function item:setColor(r, g, b)
+	if not r then
+		reaper.SetMediaItemInfo_Value(self, 'I_CUSTOMCOLOR', 0)
+		return
+	end
+	if type(r) == 'table' then r, g, b = r[1], r[2], r[3] end
+	
+	reaper.SetMediaItemInfo_Value(self, 'I_CUSTOMCOLOR', reaper.ColorToNative(
+		math.floor(r * 255 + 0.5),
+		math.floor(g * 255 + 0.5),
+		math.floor(b * 255 + 0.5)
+	) | 0x01000000)
+end
+
 function item:delete()
 	reaper.DeleteTrackMediaItem(self:getTrack(), self)
 end
